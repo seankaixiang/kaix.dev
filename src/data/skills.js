464cc7100abb63c8315afce6bc2255de517c8a16
seankaixiang <kxakai@gmail.com>
2020-12-09 13:47:50 +0000

    add skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,22 +1,32 @@
 export default [
   {
     label: "Web Development Skills",
-    description: "Skills I learned during the quarantine",
+    description: "Skills I learned",
     categories: [
       {
         title: "Front-end Development",
         skills: [
-          "React: CRA, Gatsby, and Next",
-          "Tailwind CSS and Component Libraries",
-          "Jest & React Testing Library",
-          "Design Systems w/Storybook",
-          "SEO-friendly and Accessible Designs",
-        ],
+          "HTML / CSS",
+          "Javascript / jQuery",
+          "Bootstrap",
+          "Jekyll / Gatsby / Hugo",
+          "SEO-friendly",
+          "Agolia",
+          "Google AD manager"
+        ]
       },
       {
         title: "Backend Development",
-        skills: ["Express", "Socket.io", "Firebase and Firestore"],
+        skills: ["PHP", "Laravel", "Wordpress", "MySQL", "MongoDB"]
       },
+      {
+        title: "DevOps",
+        skills: ["Google Cloud Plateform", "Apache", "Debian / Ubuntu"]
+      },
+      {
+        title: "Tool",
+        skills: ["Vagrant", "Visual Studio Code"]
+      }
     ],
   },
 ]
