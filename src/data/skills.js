e15fbf6aabebf9b84f2f05d8bb25fa00ec87b509
seankaixiang <kxakai@gmail.com>
2020-12-23 13:09:35 +0000

    update skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -4,28 +4,43 @@ export default [
     description: "Skills I learned",
     categories: [
       {
+        title: "Programming Languages",
+        skills: ["PHP", "Javascript"]
+      },
+      {
         title: "Front-end Development",
         skills: [
-          "HTML / CSS / Bootstrap",
-          "Javascript / jQuery",
-          "Agolia",
-          "Google AD manager",
+          "HTML5",
+          "CSS3",
+          "Bootstrap",
+          "jQuery",
           "Progress Web App (PWA)",
-          "Accelerated Mobile Pages (AMP)",
-          "Markdown"
+          "Accelerated Mobile Pages (AMP)"
         ]
       },
       {
+        title: "Database",
+        skills: ["MySQL", "MongoDB", "MariaDB"]
+      },
+      {
         title: "Back-end Development",
-        skills: ["PHP / Laravel / Twig", "MySQL", "MongoDB"]
+        skills: ["Laravel", "Twig", "Apache"]
       },
       {
         title: "DevOps",
-        skills: ["Google Cloud Plateform", "Debian / Ubuntu", "Apache", "Git"]
+        skills: ["Google Cloud Plateform", "Vagrant"]
+      },
+      {
+        title: "Software",
+        skills: ["Postman", "Visual Studio Code"]
+      },
+      {
+        title: "Static Site Generators",
+        skills: ["Gatsby", "Hugo", "Jekyll"]
       },
       {
         title: "Other",
-        skills: ["Vagrant / VirtualBox", "Visual Studio Code", "Swagger / OpenAPI"]
+        skills: ["Linux / Debian / Ubuntu", "Git", "Markdown", "Google AD manager", "Agolia"]
       }
     ],
   },
